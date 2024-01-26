22080d2796414bff3482982acd837a3e50e49a0f
German Arutyunov <germanarutyunov@gmail.com>
2024-01-26 14:45:54 +0400

    export routines client

--- sdk/src/commonMain/kotlin/com/simplyfi/sdk/clients/RoutinesClient.kt
+++ sdk/src/commonMain/kotlin/com/simplyfi/sdk/clients/RoutinesClient.kt
@@ -4,14 +4,20 @@ import com.simplyfi.sdk.ClientConfig
 import com.simplyfi.sdk.models.core.RunnableResult
 import com.simplyfi.sdk.models.core.routines.Routine
 import com.simplyfi.sdk.models.core.routines.RoutineExecute
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
+@OptIn(ExperimentalJsExport::class)
+@JsExport
 class RoutinesClient(config: ClientConfig) {
     private val httpClient = HttpClient(config, "v1/routines/")
 
+    @JsExport.Ignore
     suspend fun get(name: String, token: String? = null): Routine {
         return httpClient.get(name, token)
     }
 
+    @JsExport.Ignore
     suspend fun execute(
         name: String,
         payload: RoutineExecute,
@@ -20,6 +26,7 @@ class RoutinesClient(config: ClientConfig) {
         return httpClient.post("$name/execute", payload, token)
     }
 
+    @JsExport.Ignore
     suspend fun execute(
         name: String,
         vararg params: String,
